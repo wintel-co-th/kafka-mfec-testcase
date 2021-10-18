c32846f4dd5efd5e65e948befae6eb484fee8194
root <root@jumphost.3juupdkkacheneyeemhliffi3c.ix.internal.cloudapp.net>
2021-10-18 10:00:14 +0000

    test user auth

--- kafka_user_auth.sh
+++ kafka_user_auth.sh
@@ -1,49 +1,64 @@
 #! /bin/sh
 
-kubectl get secret my-user -o jsonpath='{.data.user\.crt}' -n my-namespaces | base64 --decode > 6.3/user.crt
-kubectl get secret my-user -o jsonpath='{.data.user\.key}' -n my-namespaces | base64 --decode > 6.3/user.key
-kubectl get secret my-user -o jsonpath='{.data.user\.p12}' -n my-namespaces | base64 --decode > 6.3/user.p12
-kubectl get secret my-user -o jsonpath='{.data.user\.password}' -n my-namespaces | base64 --decode > 6.3/user.password
+kafka_nemspaces=kafka
+kafka_bootstrap=kafka-bootstrap.thdlcd3-uat.aiaazure.biz
+my_user=chaiyapon
+my_cluster=thdlcd3-uat-kafka-cluster
 
 
-## Import the entry in user.p12 into another keystore
+kubectl get secret $my_user -o jsonpath='{.data.user\.crt}' -n $kafka_nemspaces | base64 --decode > 6.3/user.crt
+kubectl get secret $my_user -o jsonpath='{.data.user\.key}' -n $kafka_nemspaces | base64 --decode > 6.3/user.key
+kubectl get secret $my_user -o jsonpath='{.data.user\.p12}' -n $kafka_nemspaces | base64 --decode > 6.3/user.p12
+kubectl get secret $my_user -o jsonpath='{.data.user\.password}' -n $kafka_nemspaces | base64 --decode > 6.3/user.password
+
+
+echo "==============Import the entry in user.p12 into another keystore=============="
 
 export USER_P12_FILE_PATH=6.3/user.p12
 export USER_KEY_PASSWORD_FILE_PATH=6.3/user.password
-export KEYSTORE_NAME=kafka-auth-keystore.jks
+export KEYSTORE_NAME=6.3/kafka-auth-keystore.jks
 export KEYSTORE_PASSWORD=foobar
 export PASSWORD=$(cat 6.3/user.password)
 
-keytool -importkeystore -deststorepass $KEYSTORE_PASSWORD -destkeystore 6.3/$KEYSTORE_NAME -srckeystore $USER_P12_FILE_PATH -srcstorepass $PASSWORD -srcstoretype PKCS12 -noprompt
+echo this is $PASSWORD
+echo this is $USER_KEY_PASSWORD_FILE_PATH
+keytool -importkeystore -deststorepass $KEYSTORE_PASSWORD -destkeystore $KEYSTORE_NAME -srckeystore $USER_P12_FILE_PATH -srcstorepass $PASSWORD -srcstoretype PKCS12 -noprompt
 
-##Extract the cluster CA certificate and password
+echo "==============Extract the cluster CA certificate and password================="
 
-kubectl get secret my-cluster-cluster-ca-cert -o jsonpath='{.data.ca\.crt}'   -n my-namespaces | base64 --decode > 6.3/ca.crt
-kubectl get secret my-cluster-cluster-ca-cert -o jsonpath='{.data.ca\.password}'   -n my-namespaces | base64 --decode > 6.3/ca.password
+kubectl get secret $my_cluster-cluster-ca-cert -o jsonpath='{.data.ca\.crt}'   -n $kafka_nemspaces | base64 --decode > 6.3/ca.crt
+kubectl get secret $my_cluster-cluster-ca-cert -o jsonpath='{.data.ca\.password}'   -n $kafka_nemspaces | base64 --decode > 6.3/ca.password
 
-##Import it into truststore
+echo "==============Import it into truststore======================================="
 
 export CERT_FILE_PATH=6.3/ca.crt
 export CERT_PASSWORD_FILE_PATH=6.3/ca.password
 export KEYSTORE_LOCATION=6.3/cacerts
 export PASSWORD=$(cat 6.3/ca.password)
+export TRUSTSTORE_PASSWORD=changeit
 
-keytool -importcert -alias strimzi-kafka-cert -file $CERT_FILE_PATH -keystore $KEYSTORE_LOCATION -keypass $PASSWORD -noprompt
+echo this is $PASSWORD
 
-##// Create properties file for Kafka CLI clients
+keytool -importcert -alias strimzi-kafka-cert -file $CERT_FILE_PATH -keystore $KEYSTORE_LOCATION -keypass $PASSWORD -deststorepass $TRUSTSTORE_PASSWORD -noprompt
+
+echo "============== Create properties file for Kafka CLI clients===================="
+$(rm -rf  6.3/client-ssl-auth.properties)
 touch  6.3/client-ssl-auth.properties
-echo "bootstrap.servers=my-cluster" >> 6.3/client-ssl-auth.properties
+echo "bootstrap.servers=$kafka_bootstrap:443" >> 6.3/client-ssl-auth.properties
 echo "security.protocol=SSL" >> 6.3/client-ssl-auth.properties
 echo "ssl.truststore.location=6.3/cacerts" >> 6.3/client-ssl-auth.properties
 echo "ssl.truststore.password=changeit" >> 6.3/client-ssl-auth.properties
 echo "ssl.keystore.location=6.3/kafka-auth-keystore.jks" >> 6.3/client-ssl-auth.properties
 echo "ssl.keystore.password=foobar" >> 6.3/client-ssl-auth.properties
-echo "ssl.key.password=$(cat 6.3/ca.password)" >> 6.3/client-ssl-auth.properties
+echo "ssl.key.password=$(cat 6.3/user.password)" >> 6.3/client-ssl-auth.properties
+
+echo "this is client-ssl-auth.properties $(cat 6.3/client-ssl-auth.properties)"
+
+echo "===============Load data to topic================================================="
+
+cat file02 | kafka_2.12-2.8.0/bin/kafka-console-producer.sh --broker-list $kafka_bootstrap:443 --producer.config 6.3/client-ssl-auth.properties  --topic topic-a $1
 
 
-## // Load data to topic
+echo "================comsumer data from topic==========================================" 
 
-cat file02 | kafka_2.12-2.8.0/bin/kafka-console-producer.sh --broker-list kafka-ingress:9092 --producer.config client-ssl-auth.properties  --topic topic-a $1
-
-
-
+kafka_2.12-2.8.0/bin/kafka-console-consumer.sh --bootstrap-server $kafka_bootstrap:443  --topic topic-a --consumer.config 6.3/client-ssl-auth.properties --from-beginning --timeout-ms 10000
