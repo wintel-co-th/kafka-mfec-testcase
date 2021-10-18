01ef743a77975e7b37192e07b53280f0f33bdf53
root <root@jumphost.3juupdkkacheneyeemhliffi3c.ix.internal.cloudapp.net>
2021-10-18 08:46:45 +0000

    clean all

--- 2.2/insert.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-cat /home/kafka/file01 | /opt/kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic topic-a $1
--- connector/jdbc/dbscrip/insertdata.sql
+++
@@ -1,3 +0,0 @@
-insert into football_players (name, nationality, is_retired) values ('Andrea Pirlo','Italian', true);
-insert into football_players (name, nationality, is_retired) values ('Cristiano Ronaldo','Portuguese', false);
-insert into football_players (name, nationality, is_retired) values ('Megan Rapinoe','American', true);
